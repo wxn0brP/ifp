c020522c9fda2878f6486fb74788c09d6b90fc31
wxn0brP <toja.filiper@gmail.com>
2023-12-22 23:36:02 +0100

    star - easter egg

--- ifp/public/app/js/egg.js
+++ ifp/public/app/js/egg.js
@@ -76,3 +76,104 @@ document.querySelector("#goMain").on("dblclick", () => {
     confetti.setPower(cw.rand(15, 30));
     emulateConfettiClick(cw.rand(0, window.innerWidth), cw.rand(0, window.innerHeight));
 });
+
+(function(){
+    // orginal code: https://codepen.io/MinzCode/pen/dyOgadW
+    let canvas, ctx, w, h, particles = [];
+
+    function init(){
+        canvas = document.querySelector("#particles");
+        ctx = canvas.getContext("2d");
+        resizeReset();
+        animationLoop();
+    }
+
+    function resizeReset(){
+        w = canvas.width = window.innerWidth;
+        h = canvas.height = window.innerHeight;
+    }
+
+    function mousemove(e){
+        if(e.buttons != 2) return; // 2 = PPM
+        particles.push(new Particle(e.x, e.y));
+    }
+
+    function animationLoop(){
+        ctx.clearRect(0, 0, w, h);
+        ctx.globalCompositeOperation = 'lighter';
+
+        drawScene();
+        arrayCleanup();
+        requestAnimationFrame(animationLoop);
+    }
+
+    function arrayCleanup(){
+        let dump = [];
+        particles.map((particle) => {
+            if(particle.radius > 0)
+                dump.push(particle);
+        });
+        particles = dump;
+    }
+
+    function drawScene() {
+        particles.map((particle) => {
+            particle.update();
+            particle.draw();
+        })
+    }
+
+    class Particle{
+        constructor(x, y){
+            this.x = x;
+            this.y = y;
+            this.radius = 3;
+            this.size = 0;
+            this.rotate = 0;
+            this.alpha = 0.5;
+        }
+        setPoint(){
+            let r, x, y;
+            this.point = [];
+            for(let a = 0; a < 360; a += 36){
+                let d = ((a / 36) % 2 === 0) ? this.size : this.size / 2;
+                r = (Math.PI / 180) * (a + this.rotate);
+                x = this.x + d * Math.sin(r);
+                y = this.y + d * Math.cos(r);
+                this.point.push({x: x, y: y, r: this.radius});
+            }
+        }
+        draw(){
+            if(this.radius <= 0) return;
+            this.point.map((p) => {
+                ctx.beginPath();
+                ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+                ctx.fillStyle = `hsla(5, 100%, 50%, ${this.alpha})`;
+                ctx.fill();
+                ctx.closePath();
+            });
+
+            ctx.beginPath();
+            for(let i = 0; i < this.point.length; i++){
+                if(i === 0)
+                    ctx.moveTo(this.point[i].x, this.point[i].y);
+                else 
+                    ctx.lineTo(this.point[i].x, this.point[i].y);
+            }
+            ctx.closePath();
+            ctx.strokeStyle = `hsla(5, 100%, 50%, ${this.alpha})`;
+            ctx.stroke();
+        }
+        update(){
+            this.setPoint();
+            this.radius -= .04;
+            this.size += .7;
+            this.rotate -= 2;
+            this.alpha = (this.radius * 0.5 < 0.5) ? this.radius * 0.5 : 0.5;
+        }
+    }
+
+    window.addEventListener("resize", resizeReset);
+    window.addEventListener("mousemove", mousemove);
+    setTimeout(init, 100);
+})();
